5260b636177c3164289c5daf9b203a65f0408759
MGR <mardubudi@Govinds-MacBook-Air.local>
2018-11-03 11:59:31 +0530

    Initial commit

---
+++ src/main/java/com/micro/pro/MicroServiceProApplication.java
@@ -0,0 +1,14 @@
+package com.micro.pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MicroServiceProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceProApplication.class, args);
+	}
+}
